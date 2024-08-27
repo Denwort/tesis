20dff45c40cfa1dd90a0a384ce16e897ef565bf6
Piero Rozas <chicha@Pieros-MacBook-Air.local>
2024-08-27 00:27:27 -0500

    xd

--- README.md
+++ README.md
@@ -1,29 +1,31 @@
-San Miguel
-Pueblo libre
-Magdalena
-Jesus Maria
-Lince
-San Isidro
-Miraflores
-Surquillo
-San Borja
-Santiago de Surco
-Barranco
+Distritos:
+- San Miguel
+- Pueblo libre
+- Magdalena
+- Jesus Maria
+- Lince
+- San Isidro
+- Miraflores
+- Surquillo
+- San Borja
+- Santiago de Surco
+- Barranco
 
 Datos de:
 - link
+- referencia (nombre del proyecto / nombre de inmobiliaria)
 - latitud
 - longitud
 - direccion
-- distrito (lugar)
-- etapa
-- "fecha de entrega"
-- "financiamiento"
-- areas comunes : listado
+- distrito
+- etapa ("en construccion", "en planos", "entrega inmediata")
+- fecha_entrega (fecha de entrega)
+- financiamiento (banco que financia)
+- areas comunes (listado de areas comunes o facilidades)
 
 - tipo 
-- piso
+- pisos
 - dormitorios
 - area
-- precio
-- moneda
+- precio (con moneda)
+
--- nexoinmobiliario2.py
+++ nexoinmobiliario2.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 import csv
 import time
 import random
+import traceback
 
 user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
@@ -21,20 +22,82 @@ service = webdriver.ChromeService(executable_path='chromedriver.exe')
 resultados = []
 
 driver = webdriver.Chrome(service=service, options=options)
-wait = WebDriverWait(driver, 100)
+
+def check_element_exists(driver, by, value):
+    try:
+        driver.find_element(by, value)
+        return True
+    except Exception:
+        return False
 
 with open('nexoinmobiliaria.csv', 'r', encoding='utf-8') as csvfile:
     lines = csvfile.readlines()
+
     for line in lines[1:]:
         row = line.strip().split(",", 1)
         resultados.append((row[0], row[1]))
 
-        link = row[0]        
+        link = row[0]   
+        
         driver.get(link)
-        direccion = wait.until(EC.presence_of_element_located((By.ID, "search-projects-normales"))) 
+        
+        try:
 
+            header = driver.find_element(By.XPATH, '//*[@id="cont_ficha_desktop"]/article/header')
+            referencia = header.find_element(By.XPATH, './div[1]/h1').text
+            direccion = header.find_element(By.XPATH, './div[1]/p[1]').text 
+            location = header.find_element(By.XPATH, './div[1]/p[2]').text
+            direccion_parts = location.split("- ")
+            distrito = direccion_parts[1] if len(direccion_parts) > 1 else location
+            latitud = driver.find_element(By.ID, 'latitude').get_attribute('value')
+            longitud = driver.find_element(By.ID, 'longitude').get_attribute('value')
 
-print(len(resultados))
+            informacion = driver.find_element(By.XPATH, '//*[@id="cuadro_fix"]/div/table')
+            etapa = informacion.find_element(By.XPATH, './tbody/tr[4]/td[2]').text
+            fecha_entrega = informacion.find_element(By.XPATH, './tbody/tr[5]/td[2]').text
+            financiamiento = informacion.find_element(By.XPATH, './tbody/tr[6]/td[2]').text
+
+            ul = driver.find_element(By.CSS_SELECTOR, 'ul.Project-areas-list')
+            li = ul.find_elements(By.TAG_NAME, 'li')
+            areas_comunes = [li.text for li in li]
+
+            nav_flat_exists = check_element_exists(driver, By.ID, 'nav-flat')
+            nav_duplex_exists = check_element_exists(driver, By.ID, 'nav-duplex')
+
+            if nav_flat_exists:
+
+                nav_flat = driver.find_element(By.ID, 'nav-flat')
+                flats = nav_flat.find_elements(By.CLASS_NAME, 'Project-available-model')
+
+                for flat in flats:
+
+                    tipo = flat.find_element(By.XPATH, './/span[contains(@class, "name_tipology")]').text
+                    pisos = flat.find_element(By.CSS_SELECTOR, 'span.num_pisos').text
+                    dormitorios = flat.find_element(By.CSS_SELECTOR, 'span.bedroom').text
+                    area = flat.find_element(By.CSS_SELECTOR, 'span.area').text
+                    precio = flat.find_element(By.CSS_SELECTOR, 'span.price').text
+
+                    resultados.append((link, referencia, latitud, longitud, direccion, distrito, etapa, fecha_entrega, financiamiento, areas_comunes, tipo, pisos, dormitorios, area, precio))
+
+            if nav_duplex_exists:
+                nav_duplex = driver.find_element(By.ID, 'nav-duplex')
+                flats = nav_duplex.find_elements(By.CLASS_NAME, 'Project-available-model')
+
+                for flat in flats:
+
+                    tipo = flat.find_element(By.XPATH, './/span[contains(@class, "name_tipology")]').text
+                    pisos = flat.find_element(By.CSS_SELECTOR, 'span.num_pisos').text
+                    dormitorios = flat.find_element(By.CSS_SELECTOR, 'span.bedroom').text
+                    area = flat.find_element(By.CSS_SELECTOR, 'span.area').text
+                    precio = flat.find_element(By.CSS_SELECTOR, 'span.price').text
+
+                    resultados.append((link, referencia, latitud, longitud, direccion, distrito, etapa, fecha_entrega, financiamiento, areas_comunes, tipo, pisos, dormitorios, area, precio))
+
+        except Exception as e:
+            print(f"{link}")
+            #print(traceback.format_exc()) 
+        
+driver.quit()
 
 '''
 
